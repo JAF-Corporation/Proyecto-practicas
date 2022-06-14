66e5f9a217e2e6641af511c9d281673808e632ab
Adrian <carniceradrian64@gmail.com>
2022-06-14 11:05:22 +0200

    DOMAIN COMMIT

--- src/main/java/com/svalero/choom/domain/Hotel.java
+++ src/main/java/com/svalero/choom/domain/Hotel.java
@@ -1,4 +1,60 @@
 package com.svalero.choom.domain;
 
 public class Hotel {
+    private int hotelID;
+    private String name;
+    private String address;
+    private String city;
+    private float rating;
+
+    public Hotel() {
+    }
+
+    public Hotel(int hotelID, String name, String address, String city, float rating) {
+        this.hotelID = hotelID;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.rating = rating;
+    }
+
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public void setHotelID(int hotelID) {
+        this.hotelID = hotelID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
 }
--- src/main/java/com/svalero/choom/domain/Room.java
+++ src/main/java/com/svalero/choom/domain/Room.java
@@ -1,4 +1,51 @@
 package com.svalero.choom.domain;
 
 public class Room {
+    private int roomID;
+    private String type;
+    private float prive;
+    private int totalRooms;
+
+    public Room(int roomID, String type, float prive, int totalRooms) {
+        this.roomID = roomID;
+        this.type = type;
+        this.prive = prive;
+        this.totalRooms = totalRooms;
+    }
+
+    public Room() {
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getPrive() {
+        return prive;
+    }
+
+    public void setPrive(float prive) {
+        this.prive = prive;
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public void setTotalRooms(int totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+
 }
